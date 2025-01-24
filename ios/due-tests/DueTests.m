2516de1ce1e0e921fc6a2dc6507cab15621d5f66
Mohammad Rad <argo.mohrad@gmail.com>
2025-01-23 18:30:57 -0800

    rename

--- ios/due-tests/DueTests.m
+++ ios/due-tests/DueTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface TinyQuickstartReactNativeTests : XCTestCase
+@interface DueTests : XCTestCase
 
 @end
 
-@implementation TinyQuickstartReactNativeTests
+@implementation DueTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
