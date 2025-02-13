da09a2fe268ab77c9787e584c97139f5834143da
Mohammad Rad <argo.mohrad@gmail.com>
2025-02-12 19:28:32 -0800

    remove basic setup

--- apps/backend/src/db/queries/update.ts
+++
@@ -1,6 +0,0 @@
-import { eq } from 'drizzle-orm';
-import { db } from '../index';
-
-// export async function updatePost(id: SelectPost['id'], data: Partial<Omit<SelectPost, 'id'>>) {
-//   await db.update(postsTable).set(data).where(eq(postsTable.id, id));
-// }
